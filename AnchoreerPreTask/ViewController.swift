1b0be4233e1d4f027410b0ebe1f4bc8dbb422e33
weststar25 <jiwoo932@naver.com>
2019-06-19 11:52:52 +0900

    [ADD] .gitignore 추가

---
+++ AnchoreerPreTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnchoreerPreTask
+//
+//  Created by 김지우 on 19/06/2019.
+//  Copyright © 2019 김지우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
